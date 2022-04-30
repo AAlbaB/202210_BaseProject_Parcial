d1953ca636e1506dbfe83565c0ba6d1c39a65d40
AndresAlba <o.alba@uniandes.edu.co>
2022-04-30 16:44:26 -0500

    Tercer punto parcial

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,16 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CafeService } from '../cafe.service';
+import { Cafe } from '../cafe';
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
+  let cafe: Array<Cafe> = [];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      imports: [HttpClientModule],
+      providers: [ CafeService ]
     })
     .compileComponents();
   }));
@@ -19,10 +27,35 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    for (let i=0; i<3; i++){
+      cafe.push(
+        new Cafe(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.lorem.sentence()
+        )
+      );
+    }
+    component.cafes = cafe;
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('Should Create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Prueba muestra 3 cafes', () =>{
+    expect(component.cafes.length).toEqual(3);
+  });
+
+  it("Component has a Table", () => {
+    expect(debug.query(By.css("tbody")).childNodes.length).toBeGreaterThan(0);
   });
 });
